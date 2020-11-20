1909c6faa99731e17c336e92700656226fe78b61
AiYiNing <1914420862@qq.com>
2020-11-20 14:01:49 +0800

    Update index.js
    更新判定

--- src/helpers/base/is/index.js
+++ src/helpers/base/is/index.js
@@ -1,3 +1,7 @@
+import { Array } from "core-js"
+
+const getType = val => Object.prototype.toString.call(val)
+
 // 值类型判断 +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 export const isUndefined = val => typeof val === 'undefined'
 
@@ -5,16 +9,16 @@ export const isNull = val => val === null
 
 export const isFunction = val => typeof val === 'function'
 
-export const isArray = val => val instanceof Array
+export const isArray = val => Array.isArray ? Array.isArray(val) : getType(val) === '[object Array]'
 
 export const isRegExp = val => val instanceof RegExp
 
 export const isObject = val =>
-  typeof val === 'object' && !(isArray(val) || isNull(val))
+  typeof val === ('object' || 'function') && !isNull(val)
 
-export const isBoolean = val => typeof val === 'boolean'
+export const isBoolean = val => getType(val) === '[object Boolean]'
 
-export const isString = val => typeof val === 'string'
+export const isString = val => getType(val) === '[object String]'
 
 export const isExist = val => !(isUndefined(val) || isNull(val))
 
